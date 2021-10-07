81bcb50a81de1d2724372fabce3a8b0fb45b924f
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2021-10-07 18:28:34 +0200

    k

--- js/products.js
+++ js/products.js
@@ -1,15 +1,18 @@
-const baseUrl = "https://organicflowerpower.org/wp-json/wp/v2/posts";
+const baseUrl = "https://organicflowerpower.org/wp-json/wp/v2/posts?_embed&per_page=9";
 const productContainer = document.querySelector(".games");
 
 
 async function getProducts(url) {
     const response = await fetch(url);
+    
     const products = await response.json();
     console.log(products);
     products.forEach(function (product) {
-        productContainer.innerHTML += ` <a href="gamesdetails.html?id=${product.id}">
-            <div class="product"><h2>${product.title.rendered}</h2>
-        </div> </a>`;
+        productContainer.innerHTML += ` <div class="product"> <a href="gamesdetails.html?id=${product.id}">
+            <h2>${product.title.rendered}</h2>
+            <img class="product_image" alt="${product._embedded[`wp:featuredmedia`][`0`].alt_text}" src="${product._embedded[`wp:featuredmedia`][`0`].source_url}"/></a>
+            <a > </a>
+        </div>  `;
     })
 }
 
